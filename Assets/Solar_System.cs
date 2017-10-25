b8864da833cfe5e3d21bcb5d10bbab9d2dbda463
drinkbar <nagito12396@gmail.com>
2017-10-25 09:44:51 +0900

    태양계시물레이션(?) 드래그해서 태양과 지구의 위치를 바꾸는게 가능

---
+++ Assets/Solar_System.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solar_System : MonoBehaviour {
+    public GameObject Sun;
+    public GameObject Earth;
+
+	// Use this for initialization
+	void Start () {
+        Earth.GetComponent<Rigidbody>().AddForce(0,100,0);
+	}
+
+    // Update is called once per frame
+    void Update(){
+        //   Sun.transform.Translate(0, 0.1f, 0.1f);
+        Earth.GetComponent<Rigidbody>().AddForce(Sun.transform.position - Earth.transform.position);
+    }
+}
